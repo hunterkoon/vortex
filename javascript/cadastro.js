c359d526464eae29ce03a1bd2b4a451b0c05f916
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2020-08-23 23:55:01 -0300

    testando campos de senhas

--- javascript/cadastro.js
+++ javascript/cadastro.js
@@ -31,3 +31,43 @@ function cadastro() {
     checkPass(); checkfield();
 
 }
+
+function maiuscula () {
+    var maiuscula = document.getElementById("email");
+    maiuscula.value = maiuscula.value.toUpperCase();
+
+    function checaremail() {
+
+        if (document.forms[0].email.value ==' '
+        || document.forms[0].email.value.indexOf('@')== -1
+        || document.forms[0].email.value.indexOf('.')== -1){
+            alert("Verifique o e-mail digitado") ;
+            return false;
+        }
+    }
+    
+   checaremail();
+    
+    }; 
+
+    function alertemail (){
+
+        if (document.forms[0].email.value.length == 0) {
+            alert('Por favor, informe um e-mail');
+                document.cadastroform.email.focus();
+            return false;
+
+        }
+        return true;
+
+    }; 
+
+    function checarsenha () {
+        // testando função para validação de senhas
+    var check1 = document.getElementById('pass').value;
+    if (check1.indexOf('@')== -1){
+        document.getElementById('textpassword').innerHTML = "Senha Fraca"
+    } else document.getElementById('textpassword').innerHTML = "Senha Media"
+};
+
+
